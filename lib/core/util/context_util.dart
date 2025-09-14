4e36e4b9328ec00fe20ea5e2c6995bbc27b6aedd
Marcos Ortego <m.ortego.f@gmail.com>
2025-09-14 23:50:31 +0200

    feat(util): add snackbar shortcut to context

--- lib/core/util/context_util.dart
+++ lib/core/util/context_util.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
-/// A convenience extension on [BuildContext] for cleaner access to the theme.
+/// Provides convenient shortcuts for theme properties and UI actions on [BuildContext].
 extension ContextExt on BuildContext {
-  /// Provides simple access to the theme within the widget tree.
-  ///
-  /// Example: `color: context.colorScheme.surface`
-  /// Example: `textStyle: context.textTheme.bodyLarge`
   ThemeData get _theme => Theme.of(this);
+
+  /// Shortcut for `Theme.of(context).textTheme`.
   TextTheme get textTheme => _theme.textTheme;
+
+  /// Shortcut for `Theme.of(context).colorScheme`.
   ColorScheme get colorScheme => _theme.colorScheme;
+
+  /// Displays a simple [SnackBar] with the given [text].
+  void showSnackBar(String text) {
+    ScaffoldMessenger.of(this).hideCurrentSnackBar();
+    ScaffoldMessenger.of(this).showSnackBar(SnackBar(content: Text(text)));
+  }
 }
