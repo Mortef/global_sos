509da3c0a20a594675f07bf24310a999082a7e53
Marcos Ortego <m.ortego.f@gmail.com>
2026-04-01 14:49:09 +0200

    refactor(project): create folder structure and decouple widgets from main.dart

---
+++ lib/core/ui/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:global_sos/feature/home/home_view.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Global SOS',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomeView(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:global_sos/core/ui/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Global SOS')),
-      body: const Placeholder(),
-    );
-  }
+  runApp(const App());
 }
