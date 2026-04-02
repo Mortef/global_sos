397a63517df158b6bac6ac0be9a48cc4341863ee
Marcos Ortego <m.ortego.f@gmail.com>
2026-04-02 15:59:53 +0200

    feat: add BuildContext extensions for theme access

--- lib/core/utils/context_utils.dart
+++ lib/core/utils/context_utils.dart
@@ -8,3 +8,14 @@ extension LocalizationContextExt on BuildContext {
   /// Provides boilerplate-free access to generated localized strings.
   AppLocalizations get l10n => AppLocalizations.of(this);
 }
+
+/// ----------------------------------------------------------------------
+/// MATERIAL 3 THEME & TYPOGRAPHY
+/// ----------------------------------------------------------------------
+extension ThemeContextExt on BuildContext {
+  /// Direct access to the Material 3 ColorScheme.
+  ColorScheme get colors => Theme.of(this).colorScheme;
+
+  /// Direct access to the Material 3 TextTheme.
+  TextTheme get typography => Theme.of(this).textTheme;
+}
