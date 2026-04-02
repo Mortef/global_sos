0f2db2bcfe518f54629aeb57e06d1aa777d460a5
Marcos Ortego <m.ortego.f@gmail.com>
2026-04-02 15:13:50 +0200

    feat: setup initial internationalization configuration

--- lib/core/ui/app.dart
+++ lib/core/ui/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:global_sos/feature/home/home_view.dart';
+import 'package:global_sos/l10n/app_localizations.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -11,6 +12,8 @@ class App extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       home: const HomeView(),
     );
   }
