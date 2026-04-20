4271d8469c495e7ec35a8a915497ee9cf8c5e9c5
Marcos Ortego <m.ortego.f@gmail.com>
2026-04-20 20:06:16 +0200

    style: reformat context_utils comments

--- lib/core/utils/context_utils.dart
+++ lib/core/utils/context_utils.dart
@@ -1,21 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:global_sos/l10n/app_localizations.dart';
 
-/// ----------------------------------------------------------------------
-/// LOCALIZATION
-/// ----------------------------------------------------------------------
+/// Extension to provide quick access to localized strings.
 extension LocalizationContextExt on BuildContext {
-  /// Provides boilerplate-free access to generated localized strings.
   AppLocalizations get l10n => AppLocalizations.of(this);
 }
 
-/// ----------------------------------------------------------------------
-/// MATERIAL 3 THEME & TYPOGRAPHY
-/// ----------------------------------------------------------------------
+/// Extension to provide quick access to theme-related properties.
 extension ThemeContextExt on BuildContext {
-  /// Direct access to the Material 3 ColorScheme.
   ColorScheme get colors => Theme.of(this).colorScheme;
-
-  /// Direct access to the Material 3 TextTheme.
   TextTheme get typography => Theme.of(this).textTheme;
 }
