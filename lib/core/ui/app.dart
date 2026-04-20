08bf9847388bd7beacdc68197cf3dbf81ee068e5
Marcos Ortego <m.ortego.f@gmail.com>
2026-04-20 23:12:26 +0200

    refactor: move home_view to ui folder

--- lib/core/ui/app.dart
+++ lib/core/ui/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:global_sos/core/ui/theme/custom_theme.dart';
-import 'package:global_sos/feature/home/home_view.dart';
+import 'package:global_sos/feature/home/ui/home_view.dart';
 import 'package:global_sos/l10n/app_localizations.dart';
 
 class App extends StatelessWidget {
